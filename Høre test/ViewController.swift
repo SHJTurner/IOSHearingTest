1b676ad74772c53d61a0479c69a69a34080413fe
test7 <sttur14@tek-51524-mac.local>
2015-11-09 13:16:53 +0100

    Initial Commit

---
+++ Høre test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Høre test
+//
+//  Created by Stig Halfdan Juhl Turner on 09/11/15.
+//  Copyright © 2015 Stig Halfdan Juhl Turner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
